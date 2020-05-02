dc0e205be8c926cc607bf71676d60c0b0d3f3ae5
stanislav temelkov <stanislav.temelkov@hotmail.com>
2020-05-02 14:54:05 +0300

    Update menus and add Work time page with content

---
+++ TESTAPP/Controllers/WorkTimeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BFV_VTU.Controllers
+{
+    public class WorkTimeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var viewModel = new Models.WorkTime.WorkTimeViewModel
+            {
+                WorkDayOfWeek = new string[6] { "Moday: 08:00-19:00", "Tuesday: 08:00-19:00", "Wednesday: 08:00-19:00", "Thursday: 08:00-19:00",
+                    "Friday: 08:00-19:00", "Saturday-Sunday: 09:00-13:00" }
+            };
+
+            ViewBag.WorkDayOfWeek = viewModel.WorkDayOfWeek;
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ TESTAPP/Models/WorkTime/WorkTimeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BFV_VTU.Models.WorkTime
+{
+    public class WorkTimeViewModel
+    {
+        public string[] WorkDayOfWeek { get; set; }
+    }
+}
